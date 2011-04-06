b10801b399c7ea117937b19fa331de3204325bed
Nagyman <craig.nagy@gmail.com>
2011-04-06 15:24:48 -0400

    Initial node context_api to handle getting context data from django

--- chat/server.js
+++ chat/server.js
@@ -7,6 +7,7 @@ var http = require('http')
   , fs = require('fs')
   , io = require('socket.io')
   , channels = require('socket.io-channels')
+  , context_api = require('context_api').ContextApi()
   , sys = require(process.binding('natives').util ? 'util' : 'sys')
   , port = 8000
   , server;
@@ -36,13 +37,10 @@ channel.on('chat',function(client, msg){
 
   // Broadcast context data to all clients about this message
   // TODO: get this context from django...somehow
-  var context = {
-    message: msg,
-    postrank: {},
-    video: {},
-    image: {},
-  }
-  channel.broadcastToChannel('context', msg.channelId, context)
+
+  context_api.getContext(msg, function contextReceived(err, context) {
+      channel.broadcastToChannel('context', msg.channelId, context)
+  })
 })
 
 
