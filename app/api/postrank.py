dd67b905e8881bf78942666b3a6116ab41453b1b
Jason Peddle <jwpeddle@gmail.com>
2011-04-09 12:43:50 -0400

    updated pr api

--- app/api/postrank.py
+++ app/api/postrank.py
@@ -7,18 +7,11 @@ API_KEY = 'hackwaterloo'
 METRIC_URL = 'http://api.postrank.com/v2/entry/metrics?appkey=%s&' % (API_KEY,)
 
 class PostrankApi(Api):
-    def process(self, url):
+    def process(self, data):
         response = {}
-        try:
-            openurl = urllib.urlopen('%sid=%s' % (METRIC_URL, url))
-            content = json.loads(openurl.read())
-            response = {
-                'success': True,
-                'content': content,
-                }
-        except Exception as e:
-            response = {
-                'success': False,
-                'error_message': e.value,
-                }
-        return json.dumps(response)
+        openurl = urllib.urlopen('%sid=%s' % (METRIC_URL, id))
+        content = json.loads(openurl.read())
+        response = {
+            'postrank': content,
+            }
+        return response
