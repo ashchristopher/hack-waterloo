e43784a47208087600e75783c04d51b5b339e4f2
Ash Christopher <ash.christopher@gmail.com>
2011-04-09 14:15:30 -0400

    Updated

--- app/api/postrank.py
+++ app/api/postrank.py
@@ -7,7 +7,9 @@ API_KEY = 'hackwaterloo'
 METRIC_URL = 'http://api.postrank.com/v2/entry/metrics?appkey=%s&' % (API_KEY,)
 
 class PostrankApi(Api):
-    def process(self, data):
+    
+    @staticmethod
+    def process(data):
         openurl = urllib.urlopen('%sid=%s' % (METRIC_URL, data))
         content = json.loads(openurl.read())
         response = {
--- app/api/tests.py
+++ app/api/tests.py
@@ -7,7 +7,9 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from django.utils import simplejson
+
 from api.idee import PixMatch
+from api.pipeline import Pipeline
 
 class PixMatchTest(TestCase):
     def test_basic(self):
@@ -19,3 +21,17 @@ class PixMatchTest(TestCase):
     def test_json(self):
         ret = '{"status": "ok", "method": "search", "result": [{"score": "98.2", "filename": "/gackers_HACKWATERLOO-Y-U-NO-ENJOY-SUNLIGHT.jpg"}, {"score": "97.7", "filename": "/gackers_HACKWATERLOO-Y-U-NO-USE-POSTRANK.jpg"}, {"score": "97.6", "filename": "/gackers_HACKWATERLOO-Y-U-NO-USE-CASTROLLER.jpg"}, {"score": "97.6", "filename": "/gackers_HACKWATERLOO-Y-U-NO-USE-YELLOWPAGES.jpg"}, {"score": "97.5", "filename": "/gackers_HACKWATERLOO-Y-U-NO-USE-FRESHBOOKS.jpg"}, {"score": "96.6", "filename": "/gackers_HackWaterloo-Y-U-NO-MAKE-RESERVATION-SYSTEM.jpg"}], "error": []}'
         json = simplejson.loads(ret)
+        
+    def test_pipeline_pixmatch(self):
+        print "\n\n\n\n\n\n"
+        p = Pipeline()
+        msg = "http://dev:8000/site-media/static/images/memes/HACKWATERLOO-Y-U-NO-ENJOY-SUNLIGHT.jpg"
+        output = p.run(msg)
+        print output
+        
+    def test_pipeline_postrank(self):
+        print "\n\n\n\n\n\n"
+        p = Pipeline()
+        msg = "http://www.igvita.com/2011/04/07/life-beyond-http-11-googles-spdy/"
+        output = p.run(msg)
+        print output 
