a676d046eddfdebcfb137e7b8308c8d30f6616fe
Bartek Ciszkowski <bartekc@gap.ca>
2011-04-09 10:40:36 -0400

    added chat room template

--- static/app.js
+++ static/app.js
@@ -1,38 +1,46 @@
 var username = "User_" + new Date().getTime();
 
-var createChannel(channelId) {
-    var channel = new SocketIOChannel({
-        host: "localhost",
-        port: 8000,
-        channelId: channelId,
+var channel = new SocketIOChannel({
+    host: "localhost",
+    port: 8000,
+    channelId: "foofoo",
 
-        session: {username: username},
+    session: {username: username},
 
-        reconnectOnDisconnect: true,
-        reconnectRetryInterval: 1000 * 10 // try to reconnect every 30 seconds
+    reconnectOnDisconnect: true,
+    reconnectRetryInterval: 1000 * 10 // try to reconnect every 30 seconds
 
-        function message(obj) {
-            $("#messages").append("<div>" + obj.message[0] + "</div>");
+    function message(obj) {
+        $("#messages").append("<div>" + obj.message[0] + ", " + obj.message[1] + "</div>");
 
-        }
-        function send() {
-            var val = $("#textinput").val();
-            channel.send('chat', {message: val username: username});
-        };
+    }
 
-        channel.on('chat', function(obj) {
-            message(obj);
-        });
+    function send() {
+        var val = $("#textinput").val();
+        channel.send('chat', {message: val username: username});
+    };
 
-        channel.on('connect', function(obj) {
-            console.log('Connected!');
-
-        });
+    channel.on('chat', function(obj) {
+        message(obj);
     });
-}
+
+    channel.on('connect', function(obj) {
+        console.log('Connected!');
+
+    });
+});
 
 /*
  *
  * The front end will need to be able to specify the channel based on some output from the django
  * app?
  * */
+
+$(document).ready(function () {
+    $("#chatform").submit(function() {
+        send();
+        message({ message: ['you', val]);
+
+        return false;
+    });
+});
