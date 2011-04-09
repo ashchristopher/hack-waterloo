92948f51a35da4b5043ee5d2a97c749465a953e2
Jason Peddle <jwpeddle@gmail.com>
2011-04-09 10:38:49 -0400

    url, template

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -10,4 +11,5 @@ urlpatterns = patterns('',
 
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^$', direct_to_template, {'template': 'chat/base.html'}),
 )
