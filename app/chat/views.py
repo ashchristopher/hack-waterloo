aed3f4bfca2be38939ff1b58c4c8a922e7123d17
Ash Christopher <ash.christopher@gmail.com>
2011-04-09 10:37:05 -0400

    updated

--- app/chat/views.py
+++ app/chat/views.py
@@ -4,7 +4,11 @@ from django.template import RequestContext
 
 
 
-def chat_room(request, room_name, template='chat/rooms.html'):
+def chat_rooms_list(request, template='chat/rooms.html'):
+	context = {}
+	return render_to_response(template, context, context_instance=RequestContext(request))
+
+def chat_room(request, room_name, template='chat/chat-room.html'):
 	print room_name
 
 	context = {}
