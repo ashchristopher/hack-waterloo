872f040c7ee9ada5248e282a8eb0bbc4dab523d7
Ash Christopher <ash.christopher@gmail.com>
2011-04-09 09:06:14 -0400

    Base

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^app/', include('app.foo.urls')),
+
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
+)
