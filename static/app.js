16c68428fbe4a024836c0966c55014d89a3faa48
Bartek Ciszkowski <bartekc@gap.ca>
2011-04-09 10:33:08 -0400

    Added js frontend source files.

--- static/app.js
+++ static/app.js
@@ -1,30 +1,38 @@
 var username = "User_" + new Date().getTime();
 
-var channel = new SocketIOChannel({
-    host: "localhost",
-    port: 8000,
-    channelId: "foobar",
+var createChannel(channelId) {
+    var channel = new SocketIOChannel({
+        host: "localhost",
+        port: 8000,
+        channelId: channelId,
 
-    session: {username: username},
+        session: {username: username},
 
-    reconnectOnDisconnect: true,
-    reconnectRetryInterval: 1000 * 10 // try to reconnect every 30 seconds
+        reconnectOnDisconnect: true,
+        reconnectRetryInterval: 1000 * 10 // try to reconnect every 30 seconds
 
-    function message(obj) {
-        $("#messages").append("<div>" + obj.message[0] + "</div>");
+        function message(obj) {
+            $("#messages").append("<div>" + obj.message[0] + "</div>");
 
-    }
-    function send() {
-        var val = $("#textinput").val();
-        channel.send('chat', {message: val username: username});
-    };
+        }
+        function send() {
+            var val = $("#textinput").val();
+            channel.send('chat', {message: val username: username});
+        };
 
-    channel.on('chat', function(obj) {
-        message(obj);
+        channel.on('chat', function(obj) {
+            message(obj);
+        });
+
+        channel.on('connect', function(obj) {
+            console.log('Connected!');
+
+        });
     });
+}
 
-    channel.on('connect', function(obj) {
-        console.log('Connected!');
-
-    });
-});
+/*
+ *
+ * The front end will need to be able to specify the channel based on some output from the django
+ * app?
+ * */
